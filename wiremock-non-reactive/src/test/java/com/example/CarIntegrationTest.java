ab42a2f569dc779d3a2ce051b332de1b458faec0
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-03 10:05:22 +0000

    upgrade wiremock sample to SB3

--- wiremock-non-reactive/src/test/java/com/example/CarIntegrationTest.java
+++ wiremock-non-reactive/src/test/java/com/example/CarIntegrationTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 
